74cdd5f4321a58a3f8b65f2b48386fe825b0ce35
Antonio Victor <vgoncalvescarvalho@outlook.com>
2022-12-10 14:04:27 -0300

    Arquivos dos estudos

---
+++ ExercicioEncapsulamentoCSharpe/Program.cs
@@ -0,0 +1,63 @@
+﻿using AulasDeCsharpe2;
+using System;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("How many employees will be registered? ");
+            int number = int.Parse(Console.ReadLine());
+
+            List<Employee> funcionarios = new List<Employee>();
+
+            for (int i = 0; i < number; i++)
+            {
+                int z = 1;
+                z = z + i;
+                Console.WriteLine("Employee #" + z + ":");
+
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                funcionarios.Add(new Employee(id, name, salary));
+
+            }
+
+            Console.Write("Enter the employee id that will have salary increase:");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employee increase = funcionarios.Find(x => x.Id == searchId);
+
+            if(increase != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                increase.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This Id does not exist!");
+            }
+
+            Console.WriteLine("Updated list of employees:");
+            foreach  (Employee obj in funcionarios)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
