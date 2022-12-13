321f7207461992586044dbea22bd1d7e08351a0f
Antonio Victor <vgoncalvescarvalho@outlook.com>
2022-12-12 23:58:41 -0300

    Primeira classe de enumeração

---
+++ Capítulo de Enumerações e Composição/EnumeracoesEComposicoes/Program.cs
@@ -0,0 +1,25 @@
+﻿using Activity.Entities;
+using Activity.Entities.Enums;
+using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order()
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+            string txt = OrderStatus.PendingPayment.ToString();/* conversão do tipo string para o tipo enum*/
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");/*conversão do tipo enum para o tipo string*/
+            Console.WriteLine(os);
+        }
+    }
+}
---
+++ Exercício de Arrays/Exercício de Arrays/Exercício de Arrays/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_de_Arrays
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            Salary = Salary + Salary * percentage / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
